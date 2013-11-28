547ee14a78180bde9d4b8fa679307587ae789bbd
Atsushi Sadakiyo <s1531028@eng.kitakyu-u.ac.jp>
2013-11-28 12:50:30 +0900

    second

--- calc1/ViewController.h
+++ calc1/ViewController.h
@@ -11,8 +11,24 @@
 @interface ViewController : UIViewController
 @property(nonatomic,assign)NSInteger countNumber;
 @property(nonatomic,assign)NSInteger buttonNumber;
+@property(nonatomic,assign)NSInteger subtotal;
+@property(nonatomic,assign)NSInteger calcFlug;
 @property (weak, nonatomic) IBOutlet UILabel *numberOutput;
 - (IBAction)inputNumber1:(id)sender;
 - (IBAction)inputNumber2:(id)sender;
+- (IBAction)inputNumber3:(id)sender;
+- (IBAction)inputNumber4:(id)sender;
+- (IBAction)inputNumber5:(id)sender;
+- (IBAction)inputNumber6:(id)sender;
+- (IBAction)inputNumber7:(id)sender;
+- (IBAction)inputNumber8:(id)sender;
+- (IBAction)inputNumber9:(id)sender;
+- (IBAction)inputNumber0:(id)sender;
+- (IBAction)add:(id)sender;
+- (IBAction)hiki:(id)sender;
+- (IBAction)kake:(id)sender;
+- (IBAction)wari:(id)sender;
+- (IBAction)ans:(id)sender;
+- (IBAction)clear:(id)sender;
 
 @end
--- calc1/ViewController.m
+++ calc1/ViewController.m
@@ -16,6 +16,8 @@
 
 @synthesize countNumber;
 @synthesize buttonNumber;
+@synthesize subtotal;
+@synthesize calcFlug;
 
 - (void)viewDidLoad
 {
@@ -28,7 +30,7 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+//数字ボタン
 - (IBAction)inputNumber1:(id)sender {
     self.buttonNumber=1;
     [self labelOutput];
@@ -39,9 +41,113 @@
     [self labelOutput];
 }
 
+- (IBAction)inputNumber3:(id)sender {
+    self.buttonNumber=3;
+    [self labelOutput];
+}
+
+- (IBAction)inputNumber4:(id)sender {
+    self.buttonNumber=4;
+    [self labelOutput];
+}
+
+- (IBAction)inputNumber5:(id)sender {
+    self.buttonNumber=5;
+    [self labelOutput];
+}
+
+- (IBAction)inputNumber6:(id)sender {
+    self.buttonNumber=6;
+    [self labelOutput];
+}
+
+- (IBAction)inputNumber7:(id)sender {
+    self.buttonNumber=7;
+    [self labelOutput];
+}
+
+- (IBAction)inputNumber8:(id)sender {
+    self.buttonNumber=8;
+    [self labelOutput];
+}
+
+- (IBAction)inputNumber9:(id)sender {
+    self.buttonNumber=9;
+    [self labelOutput];
+}
+
+- (IBAction)inputNumber0:(id)sender {
+    self.buttonNumber=0;
+    [self labelOutput];
+}
+
+
 -(void)labelOutput{
     self.countNumber=(self.countNumber*10)+self.buttonNumber;
     NSString*print=[[NSString alloc]initWithFormat:@"%d",self.countNumber];
-                    self.numberOutput.text=print;
+    self.numberOutput.text=print;
 }
+
+
+//四則演算
+- (IBAction)add:(id)sender {
+    [self calc];
+    self.calcFlug=1;
+    
+}
+
+- (IBAction)hiki:(id)sender {
+    [self calc];
+    self.calcFlug=2;
+}
+
+- (IBAction)kake:(id)sender {
+    [self calc];
+    self.calcFlug=3;
+}
+
+- (IBAction)wari:(id)sender {
+    [self calc];
+    self.calcFlug=4;
+}
+
+- (IBAction)ans:(id)sender {
+    [self calc];
+    self.calcFlug=0;
+    self.subtotal=0;
+}
+
+- (IBAction)clear:(id)sender {
+    self.subtotal =0;
+    self.countNumber = 0;
+    self.calcFlug =0;
+    self.buttonNumber = 0;
+    
+    [self labelOutput];
+}
+
+//計算処理
+-(void)calc{
+    switch (calcFlug) {
+        case 1:
+            self.subtotal +=self.countNumber;
+            break;
+        case 2:
+            self.subtotal -=self.countNumber;
+            break;
+        case 3:
+            self.subtotal *=self.countNumber;
+            break;
+        case 4:
+            self.subtotal /=self.countNumber;
+            break;
+        default:
+            break;
+    }
+    self.countNumber=self.subtotal;
+    [self labelOutput];
+    self.countNumber=0;
+}
+
+
 @end
